cdc898c0e0f82308fb4bc4e8fdcdc9e089764065
jason-chueh <jason01937@gmail.com>
2023-09-04 00:50:42 +0800

    complete the filter for tinder

--- Stylish-Android-Kotlin-main/app/src/main/java/app/appworks/school/stylish/tinder/TinderFragment.kt
+++ Stylish-Android-Kotlin-main/app/src/main/java/app/appworks/school/stylish/tinder/TinderFragment.kt
@@ -10,17 +10,33 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import androidx.fragment.app.Fragment
 import app.appworks.school.stylish.R
@@ -81,6 +97,49 @@ class TinderFragment : Fragment() {
                                         }// Place this on top
                                 ) {
                                     // Add your content for the overlay here
+                                    Row(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .wrapContentHeight() // Height wraps the content
+                                            .padding(20.dp)
+                                            .align(Alignment.Center)
+                                    ) {
+                                            Icon(
+                                                Icons.Default.Close,
+                                                contentDescription = "",
+                                                tint = peachPink,
+                                                modifier = Modifier.size(50.dp)
+                                            )
+                                        Spacer(modifier = Modifier.weight(1f))
+                                            Icon(
+                                                Icons.Default.FavoriteBorder,
+                                                contentDescription = "",
+                                                tint = green10,
+                                                modifier = Modifier.size(50.dp)
+                                            )
+                                    }
+                                    val align= BiasAlignment(0f, -0.3f)
+                                    Row(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .wrapContentHeight() // Height wraps the content
+                                            .padding(10.dp)
+                                            .align(align)
+                                    ) {
+                                        Icon(
+                                            Icons.Default.ArrowBack,
+                                            contentDescription = "",
+                                            tint = peachPink,
+                                            modifier = Modifier.size(70.dp)
+                                        )
+                                        Spacer(modifier = Modifier.weight(1f))
+                                        Icon(
+                                            Icons.Default.ArrowForward,
+                                            contentDescription = "",
+                                            tint = green10,
+                                            modifier = Modifier.size(70.dp)
+                                        )
+                                    }
                                 }
                             }
                         }
